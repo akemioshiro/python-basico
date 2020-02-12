5ada3b22dc7427bd7df0fddb1da434e54136b42d
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2020-02-11 21:56:29 -0300

    inclusao de comentarios

--- aula1.py
+++ aula1.py
@@ -1,6 +1,16 @@
+# cometary
 print('teste')
 # print('teste')
 print('teste')
 print('teste')  # falando do que se trata essa linha
 # outra linha
 # isto é um comentário
+
+""" kajsdkjas
+"""
+print('comentario aspas duplas')
+
+'''
+comentarios com aspas simples
+'''
+print('comentarios aspas simples')
